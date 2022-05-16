d7f879ec7c470b76fa9ebb9dfe48e7227d3a66a5
razieldu <97752691+Razieldu@users.noreply.github.com>
2022-05-16 20:14:51 +0800

    富文本編輯器

---
+++ route.js
@@ -0,0 +1,28 @@
+const express= require("express");
+const bodyParser=require("body-parser")
+const app=express()
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/",function(req,res){
+
+
+res.sendFile(__dirname+"/index.html")
+})
+
+
+
+app.post("/",function(req,res){
+
+
+console.log(req.body.post)
+
+})
+
+app.listen(3000,function(){
+
+
+  console.log("server started ")
+})
